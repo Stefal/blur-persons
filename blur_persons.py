5689b7294a325932448caf9b8ffd105d763d1dff
Stefal <stephane.peneau@wanadoo.fr>
2022-01-27 14:45:39 +0100

    remove Path type for piexif
    Workaround for #2

--- blur_persons.py
+++ blur_persons.py
@@ -224,7 +224,7 @@ def save_and_copy_exif(image, source_path, save_path, *args, **kwargs):
     base, ext = os.path.splitext(save_path)
     tmp_path = base + ".tmp" + ext
     image.save(tmp_path, *args, **kwargs)
-    piexif.transplant(source_path, tmp_path)
+    piexif.transplant(str(source_path), str(tmp_path))
     shutil.move(os.path.abspath(tmp_path), os.path.abspath(save_path))
 
 def split_area(area_width, area_height, box_width, box_height, is_360=None, overlap_factor=0.15):
